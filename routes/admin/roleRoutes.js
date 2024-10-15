8578185149f9a6fab96833d8b1be405a8c19897f
naimatofficial <naimat.official.10@gmail.com>
2024-10-15 11:22:28 +0500

    add roles routes

--- routes/admin/roleRoutes.js
+++ routes/admin/roleRoutes.js
@@ -1,15 +1,24 @@
 import express from 'express'
-import { createRole,getRoles } from '../../controllers/admin/roleController.js'
+import {
+    createRole,
+    deleteRole,
+    getRoleById,
+    getRoles,
+    updateRole,
+} from '../../controllers/admin/roleController.js'
+import { protect, restrictTo } from '../../middleware/authMiddleware.js'
 
 const router = express.Router()
 
-router.route('/').post(createRole)
- .get(getRoles)
+router
+    .route('/')
+    .post(protect, restrictTo('admin'), createRole)
+    .get(protect, restrictTo('admin'), getRoles)
 
-// router
-//     .route('/:id')
-//     .get(getRoleById)
-//     .put(updateRole)
-//     .delete( deleteRole)
+router
+    .route('/:id')
+    .get(protect, restrictTo('admin'), getRoleById)
+    .put(protect, restrictTo('admin'), updateRole)
+    .delete(protect, restrictTo('admin'), deleteRole)
 
 export default router
