fe5e66e1437b08d74c051096a04759046371b736
naimatofficial <naimat.official.10@gmail.com>
2024-10-16 21:03:29 +0500

    update product routes

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -4,11 +4,17 @@ import {
     getImageUrl,
     getProductImageUrl,
 } from '../controllers/uploadController.js'
+import { protect, restrictTo } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.get('/upload', getImageUrl)
-router.get('/upload/product', getProductImageUrl)
+router.get(
+    '/upload/product',
+    protect,
+    restrictTo('admin', 'vendor'),
+    getProductImageUrl
+)
 
 router.delete('/delete-images', deleteImages)
 
