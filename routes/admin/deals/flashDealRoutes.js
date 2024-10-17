e816e55020573fc718d38f97c2683cb318828ed6
Ahsan <ahsanzafar0033@gmail.com>
2024-10-17 04:22:32 -0700

    final update of flashdealroutes

--- routes/admin/deals/flashDealRoutes.js
+++ routes/admin/deals/flashDealRoutes.js
@@ -13,16 +13,16 @@ import {
 } from '../../../controllers/admin/deals/flashDealController.js'
 import { validateSchema } from '../../../middleware/validationMiddleware.js'
 import flashDealValidationSchema from '../../../validations/flashDealValidator.js'
-// import { protect } from '../../../middleware/authMiddleware.js'
-// import { restrictTo } from '../../../middleware/authMiddleware.js'
+import { protect } from '../../../middleware/authMiddleware.js'
+import { restrictTo } from '../../../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router
     .route('/')
     .post(
-        // protect,
-        // restrictTo('admin'),
+        protect,
+        restrictTo('admin'),
         validateSchema(flashDealValidationSchema),
         createFlashDeal
     )
@@ -32,34 +32,34 @@ router
     .route('/:id')
     .get(getFlashDealById)
     .put(
-        // protect,
-        // restrictTo('admin'),
+        protect,
+        restrictTo('admin'),
         updateFlashDeal
     )
     .delete(
-        // protect,
-        // restrictTo('admin'),
+        protect,
+        restrictTo('admin'),
         deleteFlashDeal
     )  
 
 router
     .route('/add-product/:id')
     .put(
-        // protect,
-        // restrictTo('admin'),
+        protect,
+        restrictTo('admin'),
         addProductToFlashDeal
     )
 
 router
     .route('/remove-product/:id')
     .put(
-        // protect,
-        // restrictTo('admin'),
+        protect,
+        restrictTo('admin'),
         removeProductFromFlashDeal
     )
 
-router.route('/status/:id').put(updateFlashDealStatus)
+router.route('/status/:id').put(protect, restrictTo('admin'), updateFlashDealStatus)
 
-router.route('/publish/:id').put(updatePublishStatus)
+router.route('/publish/:id').put(protect, restrictTo('admin'), updatePublishStatus)
 
 export default router
