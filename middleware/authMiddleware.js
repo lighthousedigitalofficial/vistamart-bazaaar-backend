de6c8274cc90bf770f526701a8885c322f701803
itsKiran55 <kiranraja557@gmail.com>
2024-10-19 10:04:07 +0500

    updated coupon

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,119 +1,110 @@
-import jwt from 'jsonwebtoken'
-import { getRefreshToken } from '../services/redisService.js'
+import jwt from "jsonwebtoken";
+import { getRefreshToken } from "../services/redisService.js";
 
-import { promisify } from 'util'
-import AppError from './../utils/appError.js'
-import catchAsync from './../utils/catchAsync.js'
+import { promisify } from "util";
+import AppError from "./../utils/appError.js";
+import catchAsync from "./../utils/catchAsync.js";
 
-import Customer from '../models/users/customerModel.js'
-import Seller from '../models/sellers/vendorModel.js'
-import Employee from '../models/admin/employeeModel.js'
+import Customer from "../models/users/customerModel.js";
+import Vendor from "../models/sellers/vendorModel.js";
+import Employee from "../models/admin/employeeModel.js";
 
 const models = {
-    employee: Employee,
-    admin: Employee,
-    seller: Seller,
-    customer: Customer,
-}
+  employee: Employee,
+  admin: Employee,
+  vendor: Vendor,
+  customer: Customer,
+};
 
 export const selectModelByRole = (req, res, next) => {
-    const userRole = req.user?.role?.name.toLowerCase()
-    const Model = models[userRole]
+  const userRole = req.user?.role?.name.toLowerCase();
+  const Model = models[userRole];
 
-    if (!Model) {
-        return next(new AppError('User role not recognized.', 401))
-    }
+  if (!Model) {
+    return next(new AppError("User role not recognized.", 401));
+  }
 
-    // Attach the selected model to the request object
-    req.Model = Model
-    next()
-}
+  // Attach the selected model to the request object
+  req.Model = Model;
+  next();
+};
 
 export const protect = catchAsync(async (req, res, next) => {
-    // 1) Getting token and check of it's there
-    let token
+  // 1) Getting token and check of it's there
+  let token;
 
-    if (
-        req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
-    ) {
-        token = req.headers.authorization.split(' ')[1]
-    }
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
 
-    if (!token) {
-        return next(
-            new AppError(
-                'You are not logged in! Please log in to get access.',
-                401
-            )
-        )
-    }
+  if (!token) {
+    return next(
+      new AppError("You are not logged in! Please log in to get access.", 401)
+    );
+  }
 
-    // 2) Verification token
-    const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET)
+  // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-    const { userId } = decoded
+  const { userId } = decoded;
 
-    // 3) Check token in Redis Cache
-    const refreshToken = await getRefreshToken(userId, next)
+  // 3) Check token in Redis Cache
+  const refreshToken = await getRefreshToken(userId, next);
 
-    if (!refreshToken) {
-        return next(
-            new AppError('Unfortunately, this token has already expired.', 401)
-        )
-    }
+  if (!refreshToken) {
+    return next(
+      new AppError("Unfortunately, this token has already expired.", 401)
+    );
+  }
 
-    // 4) Determine the model based on the user role in the token
-    console.log(decoded)
-    const userRole = decoded.role.name || decoded.role // Assume role is included in the token payload
-    const Model = models[userRole]
+  // 4) Determine the model based on the user role in the token
+  console.log(decoded);
+  const userRole = decoded.role.name || decoded.role; // Assume role is included in the token payload
+  const Model = models[userRole];
 
-    if (!Model) {
-        return next(new AppError('User role not recognized.', 401))
-    }
+  if (!Model) {
+    return next(new AppError("User role not recognized.", 401));
+  }
 
-    // 5) Check if user still exists
-    const currentUser = await Model.findById(userId)
+  // 5) Check if user still exists
+  const currentUser = await Model.findById(userId);
 
-    if (!currentUser) {
-        return next(
-            new AppError(
-                'The token belonging to this user does no longer exist.',
-                401
-            )
-        )
-    }
+  if (!currentUser) {
+    return next(
+      new AppError(
+        "The token belonging to this user does no longer exist.",
+        401
+      )
+    );
+  }
 
-    // 6) Check if user changed password after the token was issued (iat: issued at)
-    if (currentUser.changePasswordAfter(decoded.iat)) {
-        return next(
-            new AppError(
-                'User recently changed password! Please log in again.',
-                401
-            )
-        )
-    }
+  // 6) Check if user changed password after the token was issued (iat: issued at)
+  if (currentUser.changePasswordAfter(decoded.iat)) {
+    return next(
+      new AppError("User recently changed password! Please log in again.", 401)
+    );
+  }
 
-    // 7) GRANT ACCESS TO PROTECTED ROUTE
-    req.user = currentUser
+  // 7) GRANT ACCESS TO PROTECTED ROUTE
+  req.user = currentUser;
 
-    next()
-})
+  next();
+});
 
 // restrictTo is a Wrapper function to return the middleware function
 export const restrictTo = (...roles) => {
-    return (req, res, next) => {
-        // roles is array: ['admin']
+  return (req, res, next) => {
+    // roles is array: ['admin']
 
-        if (!roles.includes(req.user?.role?.name)) {
-            return next(
-                new AppError(
-                    'You do not have permission to perform this action.',
-                    403
-                )
-            ) // 403: Forbiden
-        }
-
-        next()
+    if (!roles.includes(req.user?.role?.name || req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action.", 403)
+      ); // 403: Forbiden
     }
-}
+
+    next();
+  };
+};
