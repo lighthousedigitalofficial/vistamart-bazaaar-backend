d6981deea68d3efdeeb6c6c42b29cd068bc4f5b0
lighthousedigitalofficial <lighthousedev2024@gmail.com>
2024-10-24 16:34:43 +0500

    Update vendorBankModel.js

--- models/sellers/vendorBankModel.js
+++ models/sellers/vendorBankModel.js
@@ -36,17 +36,10 @@ const vendorBankSchema = new mongoose.Schema({
 
 // Pre-save hook to check if the vendor exists before saving
 vendorBankSchema.pre('save', async function (next) {
-    try {
-        const exists = await checkReferenceId('Vendor', this.vendor);
-        if (!exists) {
-            return next(new Error('Vendor ID does not exist.'));
-        }
-        next();
-    } catch (err) {
-        console.error('Error during vendor existence check:', err);
-        return next(err);
-    }
-});
+    await checkReferenceId('Vendor', this.vendor)
+
+    next()
+})
 
 // Export the model
 export default sellerDbConnection.model('VendorBank', vendorBankSchema);
