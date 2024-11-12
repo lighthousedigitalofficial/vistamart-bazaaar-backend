e16a0d814e267c6f311a9fe8f187bfe12484b122
naimatofficial <naimat.official.10@gmail.com>
2024-11-12 09:58:50 +0500

    update payment page redirect and update product controller

--- routes/sellers/productRoutes.js
+++ routes/sellers/productRoutes.js
@@ -1,13 +1,11 @@
 import express from 'express'
 import {
     createProduct,
-    updateProductImages,
     getAllProducts,
     getProductById,
     deleteProduct,
     updateProductStatus,
     updateProductFeaturedStatus,
-    sellProduct,
     updateProduct,
     getProductBySlug,
 } from '../../controllers/sellers/productController.js'
@@ -19,23 +17,13 @@ const router = express.Router()
 
 router.route('/').post(protect, createProduct).get(getAllProducts)
 
-// Static routes
-router.route('/:productId/sold').get(sellProduct)
-
-router.put('/:productId/update-product-image', protect, updateProductImages)
-
 router
     .route('/:id')
     .get(getProductById)
     .put(protect, updateProduct)
     .delete(protect, deleteProduct)
 
-router.put(
-    '/status/:id',
-    protect,
-
-    updateProductStatus
-)
+router.put('/status/:id', protect, updateProductStatus)
 
 router.get('/slug/:slug', getProductBySlug)
 
